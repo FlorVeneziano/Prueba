b0cc8a0c6beb78587144637f5cba56b55021100f
Agustin Almada <almadaagustin05@gmail.com>
2022-02-26 12:00:11 -0300

    comit de prueba

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Hola que tal estoy probando gitHubFlow</h2>
+        <p>Saludos al que este leyendo esto y que encuentre trabajo pronto</p>
+        <h1>Soy Agus</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
